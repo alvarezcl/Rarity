6e22e9efb6964fa6514cc31abf95e8f9f74c06b7
egiarta1671 <egiarta1671@gmail.com>
2015-03-01 19:06:15 -0800

    Servo and Shooter

--- Servo_Motor/Servo_Motor.ino
+++ Servo_Motor/Servo_Motor.ino
@@ -9,7 +9,9 @@ void setup()
 { 
   myservo.attach(12);  // Assigns data pin to your servo object, must be digital port
   Serial.begin(9600);
+  pinMode(13,OUTPUT);
   TMRArd_InitTimer(0,20);
+  TMRArd_InitTimer(1,2000);
 } 
 
 boolean dir = true;
@@ -20,6 +22,10 @@ void loop()
     myservo.write(dir ? pos++ : pos--);
     if (pos >= 180 || pos <= 0) dir = !dir;
     TMRArd_InitTimer(0,20);
+  }
+   if (TMRArd_IsTimerExpired(1)) {
+    digitalWrite(13,digitalRead(13) == LOW ? HIGH : LOW); 
+    TMRArd_InitTimer(1,2000);
   }
 }
 
